eb0b3ab0993e446210c2f388e91174d158371b80
Fauno <pedrohenriqueramossouza@gmail.com>
2024-01-23 02:17:16 -0300

    create app static structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
+  );
+}
+
+function Counter() {
+  return (
+    <>
+      <h1>Date counter</h1>
+      <div>
+        <button>-1</button>
+        <span>Step: 0</span>
+        <button>+1</button>
+      </div>
+
+      <div>
+        <button>-1</button>
+        <span>Count: 0</span>
+        <button>+1</button>
+      </div>
+      <p>{new Date().toDateString()}</p>
+    </>
   );
 }
 
