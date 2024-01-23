713f972a335422a4a3b5b4f8159a99b057841783
Fauno <pedrohenriqueramossouza@gmail.com>
2024-01-23 03:00:42 -0300

    add step and count controls

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
@@ -9,21 +10,43 @@ function App() {
 }
 
 function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const [date, setDate] = useState(new Date());
+
+  function decreaseStep() {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  }
+
+  function increaseStep() {
+    setStep(step + 1);
+  }
+
+  function decreaseCount() {
+    setCount(count - step);
+  }
+
+  function increaseCount() {
+    setCount(count + step);
+  }
+
   return (
     <>
       <h1>Date counter</h1>
       <div>
-        <button>-1</button>
-        <span>Step: 0</span>
-        <button>+1</button>
+        <button onClick={decreaseStep}>-1</button>
+        <span>Step: {step}</span>
+        <button onClick={increaseStep}>+1</button>
       </div>
 
       <div>
-        <button>-1</button>
-        <span>Count: 0</span>
-        <button>+1</button>
+        <button onClick={decreaseCount}>-1</button>
+        <span>Count: {count}</span>
+        <button onClick={increaseCount}>+1</button>
       </div>
-      <p>{new Date().toDateString()}</p>
+      <p>{date.toDateString()}</p>
     </>
   );
 }
